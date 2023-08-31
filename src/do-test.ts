bab0b07dcce1ba9a8b4d09cd86e81ed4720023dd
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-31 16:18:16 +0600

    Use deep.await

--- src/do-test.ts
+++ src/do-test.ts
@@ -1,9 +1,9 @@
-import { insertPort,InsertPortArg } from './insert-port.js';
-import { insertRoute,InsertRouteArg } from './insert-route.js';
-import { insertSum, InsertSumArg} from './insert-sum.js';
+import { insertPort, InsertPortArg } from './insert-port.js';
+import { insertRoute, InsertRouteArg } from './insert-route.js';
+import { insertSum, InsertSumArg } from './insert-sum.js';
 import { insertTerminalKey, InsertTerminalKeyArg } from './insert-terminal-key.js';
-import { insertTerminalPassword,InsertTerminalPasswordArg  } from './insert-terminal-password.js';
-import { insertDoTest, InsertDoTestArg} from './insert-do-test.js';
+import { insertTerminalPassword, InsertTerminalPasswordArg } from './insert-terminal-password.js';
+import { insertDoTest, InsertDoTestArg } from './insert-do-test.js';
 import assert from 'assert';
 import delay from 'delay';
 import { insertNotificationUrl, InsertNotificationUrlParam } from './insert-notification-url.js';
@@ -24,8 +24,8 @@ export type DoTestArg = InsertPortArg & InsertRouteArg & InsertSumArg & InsertTe
 //   lookingForHandlerResultAttemptsCount?: number,
 //   lookingForHandlerResultDelayInMs?: number
 // }) {
-  export async function doTest(arg: DoTestArg) {
-  const { deep, port, route, sum, terminalKey, terminalPassword, containerLinkId = deep.linkId, lookingForHandlerResultAttemptsCount = 10, lookingForHandlerResultDelayInMs = 1000 ,notificationUrl} = arg;
+export async function doTest(arg: DoTestArg) {
+  const { deep, port, route, sum, terminalKey, terminalPassword, containerLinkId = deep.linkId, notificationUrl } = arg;
   await insertTerminalKey({
     deep,
     containerLinkId,
@@ -70,36 +70,34 @@ export type DoTestArg = InsertPortArg & InsertRouteArg & InsertSumArg & InsertTe
     containerLinkId,
   });
 
+  await deep.await(doTestLinkId)
+
   const resolvedTypeLinkId = await deep.id('@deep-foundation/core', 'Resolved');
   const rejectedTypeLinkId = await deep.id('@deep-foundation/core', 'Rejected');
 
   let resolvedLink;
 
-  for (let i = 0; i < lookingForHandlerResultAttemptsCount; i++) {
-    const { data: promiseTreeLinksDownToDoTestLink } = await deep.select({
-      up: {
-        parent_id: { _eq: doTestLinkId },
-        tree_id: { _eq: await deep.id('@deep-foundation/core', 'promiseTree') },
-      },
-    });
+  const { data: promiseTreeLinksDownToDoTestLink } = await deep.select({
+    up: {
+      parent_id: { _eq: doTestLinkId },
+      tree_id: { _eq: await deep.id('@deep-foundation/core', 'promiseTree') },
+    },
+  });
 
-    const rejectedLink = promiseTreeLinksDownToDoTestLink.find(
-      (link) => link.type_id === rejectedTypeLinkId
-    );
+  const rejectedLink = promiseTreeLinksDownToDoTestLink.find(
+    (link) => link.type_id === rejectedTypeLinkId
+  );
 
-    if(rejectedLink) {
-      const {data: [promiseResultLink]} = await deep.select({
-        id: rejectedLink.to_id
-      })
-      throw new Error(promiseResultLink.value.value)
-    }
-
-    resolvedLink = promiseTreeLinksDownToDoTestLink.find(
-      (link) => link.type_id === resolvedTypeLinkId
-    );
-
-    await delay(lookingForHandlerResultDelayInMs)
+  if (rejectedLink) {
+    const { data: [promiseResultLink] } = await deep.select({
+      id: rejectedLink.to_id
+    })
+    throw new Error(promiseResultLink.value.value)
   }
+
+  resolvedLink = promiseTreeLinksDownToDoTestLink.find(
+    (link) => link.type_id === resolvedTypeLinkId
+  );
 
   assert.notStrictEqual(resolvedLink, undefined)
 }
